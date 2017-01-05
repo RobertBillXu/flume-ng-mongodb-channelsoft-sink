2d02796e9fd7f7cb030150b11bc0a5060320ebdb
xumj <xumj@channelsoft.com>
2017-01-05 21:00:30 +0800

    commit

---
+++ channelsoft.sink/src/main/java/com/mongodb/channelsoft/sink/App.java
@@ -0,0 +1,13 @@
+package com.mongodb.channelsoft.sink;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
